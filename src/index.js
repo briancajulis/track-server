d1349ab161c908d2b854ab9d0fc876ef0f2f659d
Brian Robert Cajulis <brianrobertcajulis@yahoo.com>
2019-12-19 09:55:36 -0800

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require ('express');
+const mongoose = require ('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+const mongoUri = process.env.MONGO_URI;
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err)
+});
+
+app.get('/', (req, res) => {
+    res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
